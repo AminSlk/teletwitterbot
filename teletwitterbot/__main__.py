ca2d28b1b6665d072315cf98356fed3daf762d36
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2022-10-27 00:08:42 +0330

    Revert "set webhook"
    This reverts commit 61c5c89023a7201536c9514e8ac5664c61354973.

--- teletwitterbot/__main__.py
+++ teletwitterbot/__main__.py
@@ -1,13 +1,10 @@
-import asyncio
 import logging
 
-from teletwitterbot.bot import run, setup
+from teletwitterbot.bot import main
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG)
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    application = loop.run_until_complete(setup())
-    run(application)
+    main()
--- teletwitterbot/bot.py
+++ teletwitterbot/bot.py
@@ -1,15 +1,16 @@
 from telegram.ext import ApplicationBuilder, CommandHandler
 
 from teletwitterbot import commands
-from teletwitterbot.config import WEBHOOK_URL, environment, settings
+from teletwitterbot.config import environment, settings
 
 
-async def setup():
+def main():
     builder = ApplicationBuilder().token(settings["BOT_TOKEN"])
     if settings["proxy_url"]:
         builder.proxy_url(settings["proxy_url"])
         builder.get_updates_proxy_url(settings["proxy_url"])
     application = builder.build()
+
     start_handler = CommandHandler('start', commands.start)
     application.add_handler(start_handler)
 
@@ -23,20 +24,13 @@ async def setup():
     application.add_handler(showrecent_handler)
 
     if environment == "production":
-        await application.bot.setWebhook(WEBHOOK_URL,
-                                         certificate=settings["cert_path"])
-    else:
-        await application.bot.deleteWebhook()
-    return application
-
-
-def run(application):
-    if environment == "production":
-        application.run_webhook(listen="0.0.0.0",
-                                port=8443,
-                                url_path=settings["BOT_TOKEN"],
-                                key=settings["private_key_path"],
-                                cert=settings["cert_path"],
-                                webhook_url=WEBHOOK_URL)
+        application.run_webhook(
+            listen="0.0.0.0",
+            port=8443,
+            url_path=settings["BOT_TOKEN"],
+            key=settings["private_key_path"],
+            cert=settings["cert_path"],
+            webhook_url=
+            f'https://{settings["domain"]}:8443/{settings["BOT_TOKEN"]}')
     else:
         application.run_polling()
