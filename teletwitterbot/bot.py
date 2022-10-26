61c5c89023a7201536c9514e8ac5664c61354973
Amin Salarkia <mohammadamin.salarkia@sotoon.ir>
2022-10-26 21:46:48 +0330

    set webhook

--- teletwitterbot/bot.py
+++ teletwitterbot/bot.py
@@ -10,7 +10,6 @@ def main():
         builder.proxy_url(settings["proxy_url"])
         builder.get_updates_proxy_url(settings["proxy_url"])
     application = builder.build()
-
     start_handler = CommandHandler('start', commands.start)
     application.add_handler(start_handler)
 
@@ -24,12 +23,15 @@ def main():
     application.add_handler(showrecent_handler)
 
     if environment == "production":
-        application.run_webhook(
-            listen="0.0.0.0",
-            port=8443,
-            url_path=settings["BOT_TOKEN"],
-            key=settings["private_key_path"],
-            cert=settings["cert_path"],
-            webhook_url=
-            f'https://{settings["domain"]}:8443/{settings["BOT_TOKEN"]}')
-    application.run_polling()
+        webhook_url = f'https://{settings["domain"]}:8443/{settings["BOT_TOKEN"]}'
+        application.bot.setWebhook(webhook_url,
+                                   certificate=settings["cert_path"])
+        application.run_webhook(listen="0.0.0.0",
+                                port=8443,
+                                url_path=settings["BOT_TOKEN"],
+                                key=settings["private_key_path"],
+                                cert=settings["cert_path"],
+                                webhook_url=webhook_url)
+    else:
+        application.bot.deleteWebhook()
+        application.run_polling()
